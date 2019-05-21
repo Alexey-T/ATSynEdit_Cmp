6e88528494c853b482ef3af12b717dfb32906305
Alexey-t <support@uvviewsoft.com>
2019-05-21 15:39:20 +0300

    initial

---
+++ atsynedit_cmp/atsynedit_cmp_package.pas
@@ -0,0 +1,16 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit atsynedit_cmp_package;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  ATSynEdit_Form_Complete, ATSynEdit_Form_Complete_CSS, 
+  ATSynEdit_Form_Complete_HTML, ATSynEdit_Form_Complete_SynWrite;
+
+implementation
+
+end.
