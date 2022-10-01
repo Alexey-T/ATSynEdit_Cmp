ead4584bd01d4770ab6a8074c2050395032fdabe
Alexey <support@uvviewsoft.com>
2022-10-02 02:24:48 +0300

    atsynedit_cmp_renderhtml.pas

--- atsynedit_cmp/atsynedit_cmp_package.pas
+++ atsynedit_cmp/atsynedit_cmp_package.pas
@@ -10,7 +10,8 @@ interface
 uses
   ATSynEdit_Cmp_Filenames, ATSynEdit_Cmp_AcpFiles, ATSynEdit_Cmp_CSS, 
   ATSynEdit_Cmp_HTML, ATSynEdit_Cmp_Form, ATSynEdit_Cmp_FileURI, 
-  ATSynEdit_Cmp_CSS_Provider, ATSynEdit_Cmp_HTML_Provider;
+  ATSynEdit_Cmp_CSS_Provider, ATSynEdit_Cmp_HTML_Provider, 
+  ATSynEdit_Cmp_RenderHTML;
 
 implementation
 
